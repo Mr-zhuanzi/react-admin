df62c1841d68a0d99fea3e1e197e9075fb99bccd
huanzi <412878912@qq.com>
2019-02-26 22:43:43 +0800

    完成静态页面和登录页面

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+
+import  Login from './pages/login';
+import  Admin from  './pages/admin';
+import './pages/login/index.less'
+import './assets/index.less'
+
+export default class App extends Component {
+
+  render () {
+    return (
+      <Router>
+        <Switch>
+         <Route path='/login' component={Login}/>
+         <Route path='/' component={Admin}/>
+        </Switch>
+      </Router>
+    )
+  }
+}
---
+++ src/components/login-form/index.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {
+  Form,
+  Icon,
+  Input,
+  Button,
+  message
+} from 'antd';
+// 放在import下面不然报错
+ const Item = Form.Item;
+ class LocginForm extends Component {
+  render() {
+    return (
+      <Form className="login-form-container" >
+        <Item>
+          <Input   prefix={<Icon type="user" />} placeholder="请输入您的账号" />
+        </Item>
+        <Item>
+          <Input   prefix={<Icon type="safety" />} placeholder="请输入您的密码" />
+        </Item>
+        <Item>
+          <Button className="login-form-button">登录</Button>
+        </Item>
+      </Form>
+    )
+  }
+}
