78114b42c4d6fc753bda6c37206f20be436ed519
huanzi <412878912@qq.com>
2019-02-28 22:36:54 +0800

    完成登录页面的侧边导航

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 import  './index.less'
+import {Row, Col, Modal, message} from 'antd';
 export default class header extends Component {
   render() {
     return (
       <div className='header'>
-        <span>欢迎，xxx</span>
-        <a href='javascript:void (0)'>退出</a>
+        <Row className='header-top'>
+          <span>欢迎, xxxxxx</span>
+          <a href="javascript:void(0);" >退出</a>
+        </Row>
+        <Row className='header-bottom'>
+          <Col span={6} className='header-bottom-left'>啊啊啊</Col>
+          <Col span={18} className='header-bottom-right'>
+          {/*<span>{sysTime}</span>*/}
+          {/*<img src={dayPictureUrl} alt="天气"/>*/}
+          {/*<span>{weather}</span>*/}
+          啊啊啊啊啊啊啊啊啊啊啊
+          </Col>
+        </Row>
       </div>
     )
   }
