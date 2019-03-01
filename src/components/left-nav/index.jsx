730d771a0ab445a67520f6961dee617d047f841e
huanzi <412878912@qq.com>
2019-03-01 10:38:54 +0800

    完成自动生成菜单

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -9,17 +9,25 @@ import logo from '../../assets/images/logo.png';
 
 const SubMenu = Menu.SubMenu;
 const Item = Menu.Item;
-export default class LeftNav extends Component {
+class LeftNav extends Component {
   render() {
+    // 获取当前的路径
+    const {pathname} = this.props.location;
     return (
       <div className='left-nav'>
+        <header>
         <NavLink to='/home' className='left-nav-header'>
           <img src={logo} alt="logo"/>
           <h2>硅谷后台</h2>
         </NavLink>
-        <Menu  theme="dark" mode="inline">
+        </header>
+        <Menu
+          theme="dark"
+          mode="inline"
+          selectedKeys={[pathname]}
+        >
 
-          <Item>
+          <Item key='/home'>
             <NavLink to='/home'>
               <Icon type="home" />
               <span>首页</span>
@@ -32,45 +40,45 @@ export default class LeftNav extends Component {
               商品
             </span>
           }>
-            <Item>
+            <Item  key='/category'>
               <NavLink to='/category'>
               <Icon type="database" theme="filled" />
               <span>品类管理</span>
               </NavLink>
-            </Item>
-            <Item>
+            </Item  >
+            <Item  key='/product'>
               <NavLink to='/product'>
               <Icon type="tool" theme="filled" />
               <span>商品管理</span>
             </NavLink>
             </Item>
         </SubMenu>
-          <Item>
+          <Item  key='/user'>
             <NavLink to='/user'>
             <Icon type="user" />
             <span>用户管理</span>
             </NavLink>
           </Item>
-          <Item>
+          <Item  key='/role'>
             <NavLink to='/role'>
             <Icon type="safety-certificate" />
             <span>权限管理</span>
           </NavLink>
           </Item>
           <SubMenu title={<span><Icon type="area-chart" />图形图表</span>}>
-          <Item>
+          <Item  key='/charts'>
             <NavLink to='/charts'>
             <Icon type="bar-chart" />
             <span>柱形图</span>
             </NavLink>
           </Item>
-          <Item>
+          <Item  key='7'>
             <NavLink to='/charts'>
             <Icon type="line-chart" />
             <span>折线图</span>
           </NavLink>
           </Item>
-            <Item>
+            <Item  key='8'>
               <NavLink to='/charts'>
               <Icon type="line-chart" />
               <span>饼图</span>
@@ -84,3 +92,6 @@ export default class LeftNav extends Component {
     )
   }
 }
+
+// withRouter 是一个高阶组件
+export default withRouter(LeftNav)
